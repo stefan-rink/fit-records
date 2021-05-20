f5fd086160b7d6e2273bd5e275119290ef71f7fe
Stefan Rink <rinkyy.sr@googlemail.com>
2021-05-20 19:42:30 +0200

    feat(*): Initialize project

---
+++ src/views/Home.vue
@@ -0,0 +1,33 @@
+<template>
+  <header>
+    <h1>Today</h1>
+  </header>
+  <nav class="bottom-toolbar">
+    Add
+  </nav>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'Home',
+  components: {},
+});
+</script>
+
+<style scoped lang="less">
+h1 {
+  margin: 40px 0 0;
+}
+
+.bottom-toolbar {
+  position: fixed;
+  bottom: 0;
+  right: 0;
+  left: 0;
+  height: 64px;
+  background: #e4e4e4;
+}
+
+</style>
