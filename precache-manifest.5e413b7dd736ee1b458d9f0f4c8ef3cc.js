ff4368210c8caaf756b1de9e1f80f7270f4536a8
stefan-rink <rinkyy.sr@googlemail.com>
2022-07-10 17:13:02 +0000

    gh-pages

---
+++ precache-manifest.5e413b7dd736ee1b458d9f0f4c8ef3cc.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ab623f57031cee66c6743e0ec7f288a",
+    "url": "/fit-records/app.webmanifest"
+  },
+  {
+    "revision": "e9b180e5dbc79de341a9",
+    "url": "/fit-records/css/app.6a06824d.css"
+  },
+  {
+    "revision": "8b6af8e5e8324edfd77af8b3b35d7f9c",
+    "url": "/fit-records/fonts/Poppins-Regular.8b6af8e5.ttf"
+  },
+  {
+    "revision": "cfa9dd56f85ae0a65fbfb27511605a34",
+    "url": "/fit-records/img/icons.cfa9dd56.svg"
+  },
+  {
+    "revision": "5d667ef23f69614bb399aa1d151cc875",
+    "url": "/fit-records/img/undraw_empty.5d667ef2.svg"
+  },
+  {
+    "revision": "a31433cca25a52a2800297edc2969801",
+    "url": "/fit-records/index.html"
+  },
+  {
+    "revision": "e9b180e5dbc79de341a9",
+    "url": "/fit-records/js/app.c2af53dc.js"
+  },
+  {
+    "revision": "ce1265bde0725a61d842",
+    "url": "/fit-records/js/chunk-vendors.43105228.js"
+  },
+  {
+    "revision": "370efbbfbe1a8767611d3780a2d4cdb7",
+    "url": "/fit-records/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/fit-records/robots.txt"
+  }
+]);
